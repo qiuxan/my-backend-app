b034205af4c59a1d19486992c106b51bff8acf13
qiuxan <qiuxan@gmail.com>
2025-10-03 07:41:23 +1000

    Query Get parameters

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,8 +1,11 @@
-import express from 'express';
+import express, { query } from 'express';
 
 const userRouter = express.Router();
 
 userRouter.get('/register', (req, res) => {
+
+  console.log('testing: http://localhost:3000/users/register?m=development&n=123');
+  console.log({query: req.query}); 
   res.json({ message: 'User registration route' });
 });
 
