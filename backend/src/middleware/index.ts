a986eb9ba07a077f0c40cb48da47b47865c3e916
qiuxan <qiuxan@gmail.com>
2025-10-04 17:20:28 +1000

    Remove the project to use Express Generator

--- backend/src/middleware/index.ts
+++
@@ -1,8 +0,0 @@
-// export a express middleware 
-
-import { Request, Response, NextFunction } from 'express';
-
-export function logRequestDetails(req: Request, res: Response, next: NextFunction) {
-    console.log(`${req.method} ${req.url} - ${new Date().toISOString()}`);
-    next();
-}
